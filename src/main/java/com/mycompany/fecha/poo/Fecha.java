37f6f1431f1af2fda2f5492066d30848df962f38
Nick3030w <niconianessu@gmail.com>
2024-04-22 19:51:07 -0500

    se termino el ejercicio, con sus complejidades y metodos

--- src/main/java/com/mycompany/fecha/poo/Fecha.java
+++ src/main/java/com/mycompany/fecha/poo/Fecha.java
@@ -1,44 +1,41 @@
 package com.mycompany.fecha.poo;
-import java.util.Scanner;
 import java.util.HashMap;
 public class Fecha {
-    int dia = 0;
-    int mes = 0;
-    int ano = 0;
-/**
- * Constructor de la clase
- * @param dia dia de la fecha
- * @param mes mes de la fecha
- * @param ano año de la fecha
- * 
- * complejidad temporal: 0(1) complejidad constante
- */
-public Fecha(int dia, int mes, int ano){
-    Scanner lector = new Scanner(System.in);
-        this.dia = lector.nextInt();
-        this.mes = lector.nextInt();
-        this.ano = lector.nextInt();
-}
-/**
- * metodo que muestra la fecha corta
- * 
- * complejidad temporal: 0(1) complejidad constante
- */
-public String fechaCorta(){
-        return dia + "/" + mes + "/" + ano;
-
+    private int dia;
+    private int mes;
+    private int ano;
+    /**
+     * Contructor de la clase
+     * @param dia dia de la fecha
+     * @param mes mes de la fecha
+     * @param ano año de la fecha
+     * 
+     * complejidad temporal: 0(1) complejidad constante
+     */
+    public Fecha(int dia, int mes, int ano){
+        this.dia = dia;
+        this.mes = mes;
+        this.ano = ano;
     }
-/**
- * metodo que valida la fecha
- * 
- * complejidad temporal: 0(1) complejidad constante
- * @return 
- */
- public boolean validaFecha () {
+    /**
+     * metodo que muestra la fecha corta
+     * 
+     * complejidad temporal: 0(1) complejidad constante
+     *@return 
+     */
+    public String fechaCorta(){
+        return dia+"/"+mes+"/"+ano;
+    }
+    /**
+     * metodo que valida la fecha
+     * 
+     * complejidad temporal: 0(1) complejidad constante
+     * @return 
+     */
+     public boolean validaFecha () {
 
         if (mes>12 || mes<1) {
             return false;
-
         } 
         else if (mes==2)
         {
@@ -49,34 +46,43 @@ public String fechaCorta(){
         else{
 
             if ((mes==1 || mes==3 || mes==5 || mes==7 || mes ==8 || mes ==10 || mes==12) && dia>=1 && dia<=31) {
-                    return true;
-                
-                    
+                    return true;           
+            }
+
+            else if (dia>=1 && dia<=30) {
+                return true;
+            }
         }
- }return false;
- }
-/**
- * metodo que muestra la fecha con el mes en letra, es decir: 1 de enero de 2024
- * 
- * complejidad temporal: 0(1) complejidad constante
- * @param mes 
- */
-public void mesLetra(String[] mes) {
-        HashMap<String, String> letraMes = new HashMap<>();
-        letraMes.put("1", "enero");
-        letraMes.put("2", "febrero");
-        letraMes.put("3", "marzo");
-        letraMes.put("4", "abril");
-        letraMes.put("5", "mayo");
-        letraMes.put("6", "junio");
-        letraMes.put("7", "julio");
-        letraMes.put("8", "agosto");
-        letraMes.put("9", "septiembre");
-        letraMes.put("10", "octubre");
-        letraMes.put("11", "noviembre");
-        letraMes.put("12", "diciembre");
-        String string = "";
-        String mesEnLetra = letraMes.get(string+mes);
-        System.out.println(dia+" de "+mesEnLetra+" de "+ ano);
-}
+        return false;
+    } 
+    /**
+     * 
+     * complejidad temporal: 0(1) complejidad constante
+     * @return
+     */
+    public String mesLetra() {
+            HashMap<Integer, String> letraMes = new HashMap<>();
+            letraMes.put(1, "enero");
+            letraMes.put(2, "febrero");
+            letraMes.put(3, "marzo");
+            letraMes.put(4, "abril");
+            letraMes.put(5, "mayo");
+            letraMes.put(6, "junio");
+            letraMes.put(7, "julio");
+            letraMes.put(8, "agosto");
+            letraMes.put(9, "septiembre");
+            letraMes.put(10, "octubre");
+            letraMes.put(11, "noviembre");
+            letraMes.put(12, "diciembre");
+            return letraMes.get(mes);
+    }
+    /**
+     * complejidad temporal: 0(1) complejidad constante
+     * 
+     * metodo que muestra la fecha con el mes en letra, es decir: 1 de enero de 2024
+     * @return 
+     */
+    public String fechaLarga(){
+        return dia + " de " + this.mesLetra() + " de " + ano;
+    }
 }
--- src/main/java/com/mycompany/fecha/poo/Principal.java
+++ src/main/java/com/mycompany/fecha/poo/Principal.java
@@ -1,5 +1,20 @@
 package com.mycompany.fecha.poo;
 
+import java.util.Scanner;
+
 public class Principal {
-    // TODO: Aquí va el código de la clase Principal
+    public static void main(String[] args) {
+        Scanner lector = new Scanner(System.in);
+        int dia = lector.nextInt();
+        int mes = lector.nextInt();
+        int ano = lector.nextInt();
+            
+        Fecha fecha = new Fecha(dia, mes, ano);
+
+        System.out.println(fecha.fechaCorta());
+        
+        if(fecha.validaFecha() == true){
+            System.out.println(fecha.fechaLarga());
+        }
+    }
 }
